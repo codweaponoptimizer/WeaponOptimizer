14d3145d01fab69b454a959db7a4c6a8aca8c146
bserrano1 <bserrano@ggc.edu>
2020-07-01 16:54:31 -0400

    Inital push of Spring Boot Application

---
+++ cod-wo/src/test/java/com/codwo/CodWoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodWoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
